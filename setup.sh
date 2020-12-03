c3522de6f0872e6666846277b711f8d8fdb8cac7
Tiago Gouvêa <tiago@tiagogouvea.com.br>
2020-12-03 08:13:51 -0300

    First files added

---
+++ setup.sh
@@ -0,0 +1,32 @@
+
+
+# Check for config.sh
+
+# Load config
+
+# Check if running as root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+#### Run setup ####
+
+# Update system
+apt-get update
+
+# Private keys
+# Allow/Disallow password authentication
+# User setup
+
+
+# Docker setup
+docker_setup
+
+# Docker compose setup
+docker_compose_setup
+
+# Clone repository
+
+
+source helpers.sh
